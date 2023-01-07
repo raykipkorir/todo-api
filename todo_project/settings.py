8f0d2f2ca4f2df65ad447cc891ef8d522e4a7275
raykipkorir <rakipkorir02@gmail.com>
2023-01-07 13:16:44 +0530

    connected a remote postgresql db

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from dj_database_url import parse
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,14 +80,20 @@ WSGI_APPLICATION = "todo_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default":config(
+    "DATABASE_URL",
+    default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+    cast=parse
+    )
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
