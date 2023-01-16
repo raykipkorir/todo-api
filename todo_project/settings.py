9fa9c2c734843ea5746a510f3ca8157136aa1be4
Raymond Kipkorir <93333555+raykipkorir@users.noreply.github.com>
2023-01-16 09:32:36 +0300

    Added CORS allowed origins

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -139,5 +139,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS= [
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "https://todowithreact02.netlify.app",
 ]
